d867511f074ee58862520f77c79353a25d660778
Woratat Kumklam <woratat_kumklam@cmu.ac.th>
2022-05-13 15:52:34 +0700

    Product card in home component

--- bunny_ear/lib/pages/home/components/product_card.dart
+++ bunny_ear/lib/pages/home/components/product_card.dart
@@ -1,3 +1,4 @@
+import 'package:bunny_ear/constant.dart';
 import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
@@ -17,6 +18,50 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        width: 154,
+        padding: const EdgeInsets.all(8),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: bgColor,
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+              ),
+              child: Image.asset(
+                image,
+                height: 132,
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: Text(
+                  title,
+                  style: const TextStyle(color: Colors.black54),
+                )),
+                SizedBox(
+                  width: 3,
+                ),
+                Text(
+                  "THB" + price.toString(),
+                  style: Theme.of(context).textTheme.subtitle2,
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
