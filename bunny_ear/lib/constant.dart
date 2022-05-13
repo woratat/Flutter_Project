18a873a0757a361d52ca70edd2e507dd407e2966
Woratat Kumklam <woratat_kumklam@cmu.ac.th>
2022-05-13 14:12:49 +0700

    Segregrate components folder

--- bunny_ear/lib/constant.dart
+++ bunny_ear/lib/constant.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color secondColor = Color.fromRGBO(252, 237, 240, 1);
+const Color secondColor = Color.fromRGBO(253, 215, 222, 1);
+const Color thirdColor = Color.fromRGBO(252, 237, 240, 1);
 const Color primaryColor = Color.fromRGBO(255, 181, 194, 1);
 const Color bgColor = Color(0xFFFBFBFD);
