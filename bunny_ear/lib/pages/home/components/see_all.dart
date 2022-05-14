97749a65ff7df2e4a2b986ec3b6e761a0d3fc5af
pakorndiode <pakorn_sillapaprap>
2022-05-14 23:41:05 +0700

    split method seeAll

--- bunny_ear/lib/pages/home/components/see_all.dart
+++ bunny_ear/lib/pages/home/components/see_all.dart
@@ -1,11 +1,12 @@
 
-import 'dart:math';
-
 import 'package:bunny_ear/constant.dart';
 import 'package:bunny_ear/models/Product.dart';
 import 'package:bunny_ear/pages/detail/detail.dart';
 import 'package:bunny_ear/pages/home/home.dart';
 import 'package:flutter/services.dart' as rootBundle;
+import 'package:bunny_ear/pages/home/components/subPage/new_avrrival_page.dart';
+import 'package:bunny_ear/pages/home/components/subPage/popular_page.dart';
+
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -26,196 +27,15 @@ class _SeeAllPageState extends State<SeeAllPage> {
   Widget build(BuildContext context) {
 
     if(widget.title =="New Arrival"){
-      return Scaffold(
-        backgroundColor: bgColor,
-        appBar: AppBar(
-          backgroundColor: primaryColor,
-          title:Text(widget.title),
-          elevation: 0,
-          leading: IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: Icon(Icons.arrow_back_ios_rounded),
-            alignment: Alignment.center,
-            splashColor: Colors.transparent,
-          ),
-        ),
-        body: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical:4),
-            ),
-            FutureBuilder(
-              future: ReadJsonData(),
-              builder: (context,data){
-                var items = data.data as List<Product>;
-                return GridView.count(
-                  physics: NeverScrollableScrollPhysics(),
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  padding: const EdgeInsets.all(8.0),
-                  children: List.generate(items.length, (index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                          builder: (context) =>
-                            DetailPage(product: items[index]),
-                          )
-                        );
-                      },
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.2),
-                              spreadRadius: 3,
-                              blurRadius: 5
-                            )
-                          ],
-                          image: DecorationImage(
-                            image: NetworkImage(
-                            items[index].images[0]
-                            ),
-                            fit: BoxFit.cover
-                          )
-                        ),
-                        child: Center(
-                          child:Column(
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(8,135,8,5),
-                                child: Container(
-                                  color: Color(0xFFEBEBEBE),
-                                  height: 1,
-                                ),
-                              ),
-                              Text(items[index].title)
-                            ]
-                          ) ,
-                        )
-                      ),
-                    );
-                  }
-                  ),
-                );
-              }
-            )
-            
-          ],
-        ),
-      );
+      return newArrivalPage(context);
     } else if(widget.title =="Popular"){
-      return Scaffold(
-        backgroundColor: bgColor,
-        appBar: AppBar(
-          backgroundColor: primaryColor,
-          title:Text(widget.title),
-          elevation: 0,
-          leading: IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: Icon(Icons.arrow_back_ios_rounded),
-            alignment: Alignment.center,
-            splashColor: Colors.transparent,
-          ),
-        ),
-        body: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4),
-            ),
-            FutureBuilder(
-              future: ReadJsonData2(),
-              builder: (context,data){
-                var items = data.data as List<Product>;
-                return GridView.count(
-                  physics: NeverScrollableScrollPhysics(),
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  padding: const EdgeInsets.all(8.0),
-                  children: List.generate(items.length, (index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                          builder: (context) =>
-                            DetailPage(product: items[index]),
-                          )
-                        );
-                      },
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.2),
-                              spreadRadius: 3,
-                              blurRadius: 5
-                            )
-                          ],
-                          image: DecorationImage(
-                            image: NetworkImage(
-                            items[index].images[0]
-                            ),
-                            fit: BoxFit.cover
-                          )
-                        ),
-                        child: Center(
-                          child:Column(
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(8,135,8,5),
-                                child: Container(
-                                  color: Color(0xFFEBEBEBE),
-                                  height: 1,
-                                ),
-                              ),
-                              Text(items[index].title),
-                            ]
-                          ) ,
-                        )
-                      ),
-                    );
-                  }
-                  ),
-                );
-              }
-            )
-            
-          ],
-        ),
-      );
+      return popular_page(context);
     } 
     else{
       return Text("data");
     }
 
   }
-
-  Future<List<Product>> ReadJsonData() async {
-    final jsondata =
-        await rootBundle.rootBundle.loadString('assets/json/Product.json');
-    final list = json.decode(jsondata) as List<dynamic>;
-    return list.map((e) => Product.fromJson(e)).toList();
-  }
-
-  Future<List<Product>> ReadJsonData2() async {
-    final jsondata =
-        await rootBundle.rootBundle.loadString('assets/json/popularProduct.json');
-    final list = json.decode(jsondata) as List<dynamic>;
-    return list.map((e) => Product.fromJson(e)).toList();
-  }
+  
 
 }
---
+++ bunny_ear/lib/pages/home/components/subPage/new_avrrival_page.dart
@@ -0,0 +1,117 @@
+import 'package:bunny_ear/constant.dart';
+import 'package:bunny_ear/models/Product.dart';
+import 'package:bunny_ear/pages/detail/detail.dart';
+import 'package:bunny_ear/pages/home/home.dart';
+import 'package:flutter/services.dart' as rootBundle;
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+Scaffold newArrivalPage(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title:Text("New Arrival"),
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(Icons.arrow_back_ios_rounded),
+          alignment: Alignment.center,
+          splashColor: Colors.transparent,
+        ),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical:4),
+          ),
+          FutureBuilder(
+            future: ReadJsonData(),
+            builder: (context,data){
+              var items = data.data as List<Product>;
+              return GridView.count(
+                physics: NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                padding: const EdgeInsets.all(8.0),
+                children: List.generate(items.length, (index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                        builder: (context) =>
+                          DetailPage(product: items[index]),
+                        )
+                      );
+                    },
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 3,
+                            blurRadius: 5
+                          )
+                        ],
+                        image: DecorationImage(
+                          image: NetworkImage(
+                          items[index].images[0]
+                          ),
+                          fit: BoxFit.cover
+                        )
+                      ),
+                      child: Center(
+                        child:Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(8,0,8,0),
+                              child: Container(
+                                color: Color(0xFFEBEBEBE),
+                                height: 1,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 35,
+                              child: Stack(
+                                children: [
+                                  Container(decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(15),bottomLeft: Radius.circular(15)),
+                                    color :Colors.white),
+                                  ),
+                                  Center(child: Text(items[index].title),)
+                                ],
+                              ),
+                            )
+                          ]
+                        ) ,
+                      )
+                    ),
+                  );
+                }
+                ),
+              );
+            }
+          )
+          
+        ],
+      ),
+    );
+}
+
+
+  Future<List<Product>> ReadJsonData() async {
+    final jsondata =
+        await rootBundle.rootBundle.loadString('assets/json/Product.json');
+    final list = json.decode(jsondata) as List<dynamic>;
+    return list.map((e) => Product.fromJson(e)).toList();
+  }
