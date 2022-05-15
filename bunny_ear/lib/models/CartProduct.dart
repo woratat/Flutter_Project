f3e6e1e8766e7dad27b36799317a9b6862febd17
tumtumrp <ratchapol.tum@gmail.com>
2022-05-15 18:31:11 +0700

    create new model and add lib

---
+++ bunny_ear/lib/models/CartProduct.dart
@@ -0,0 +1,14 @@
+import 'package:bunny_ear/models/Product.dart';
+import 'package:flutter/material.dart';
+
+class CartProduct {
+  final Product product;
+  final int quantity;
+  final String type;
+
+  const CartProduct({
+    required this.product,
+    required this.quantity,
+    required this.type,
+  });
+}
